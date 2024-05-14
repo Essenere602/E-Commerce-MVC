70066503014639f80635aab387ffeee9c4ca0ab1
Olivier Allegret <olivier@colnem.net>
2024-05-14 16:01:30 +0200

    push push mardi di

--- App/views/ProductsByCatView.php
+++ App/views/ProductsByCatView.php
@@ -3,14 +3,18 @@ namespace Views;
 
 class ProductsByCatView {
     public function showItems($items) {
+        $session_id = 2;
+
         foreach($items as $item) {
-            echo '<p>' . $item['product_name'] . '</p>';
+            echo '<form class="product-form">';
+            echo '<label>' . $item['product_name'] . '</label>';
+            echo '<input type="hidden" name="product_id" class="product_id" value="' . $item['id'] . '">';
+            echo '<input type="hidden" name="session_id" class="session_id" value="' . $session_id . '">';
+            echo '<input type="number" name="qte" class="qte">';
+            echo '<button type="button" class="add-to-cart">Ajouter</button>';
+            echo '</form>';
         }
-        // if ($items !== null && isset($items['product_name'])) {
-        //     echo '<p>' . $item['product_name'] . '</p>';
-        //     echo '<p>' . $item['price'] . '</p>';
-        // } else {
-        //     echo "Produit non trouvé.";
-        // }
+        echo '<script src="assets/js/addToCart.js"></script>';
     }
 }
+?>
--- router.php
+++ router.php
@@ -4,6 +4,7 @@ use Controllers\UserController; // Déplacer l'instruction use en dehors du swit
 use Controllers\AdminProduct; // Déplacer l'instruction use en dehors du switch
 use Controllers\ProductShow;
 use Controllers\ProductsListByCat;
+use Controllers\CartController;
 use App\Database;
 $pdo = new Database;
 switch($_REQUEST['action'] ?? null) {
@@ -32,6 +33,11 @@ switch($_REQUEST['action'] ?? null) {
     case 'panier':
         echo 'Mon panier';
     break;
+    case 'addToCart': // Nouveau cas pour ajouter au panier
+        $cartController = new CartController();
+        $cartController->addToCart();
+    break;
+
     case 'commande':
         $step = $_REQUEST['step'] ?? null;
         switch ($step) {
