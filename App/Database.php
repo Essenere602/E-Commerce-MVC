aaf5b5a8a397857d4c31fa925572984f1591c38e
Olivier Allegret <olivier@colnem.net>
2024-05-16 16:18:10 +0200

    test

--- App/Database.php
+++ App/Database.php
@@ -1,21 +1,25 @@
 <?php
-namespace App;
 
-class Database {
+class DataBase {
     protected $cnx;
-    protected $host = 'localhost';
-    protected $db = 'eshop_mvc';
-    protected $login = 'root';
-    protected $pw = 'root';
-    
+    protected $host = "localhost";
+    protected $db= 'eshop_mvc';
+    protected $login= "root";
+    protected $pw = "root";
+
     public function __construct() {
-        $this->cnx = new \PDO("mysql:host=$this->host;dbname=$this->db", $this->login, $this->pw);
-        $this->cnx->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        
+            $this->cnx = new PDO("mysql:host={$this->host};dbname={$this->db}", $this->login, $this->pw);
+            $this->cnx->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+           
+        } 
+    public function getConnection() {
+            return $this->cnx;
+        }
     }
 
-    // Méthode publique pour récupérer la connexion à la base de données
-    public function getConnection() {
-        return $this->cnx;
-    }
-}
+  
+
+
+
 ?>
