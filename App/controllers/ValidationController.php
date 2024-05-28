7bcb090a0bc93b95b96ae6b9bc5bdc5845f29ab1
HCairo <hugo.cairoh@gmail.com>
2024-05-28 12:17:25 +0200

    validation start

---
+++ App/controllers/ValidationController.php
@@ -0,0 +1,9 @@
+<?php
+namespace Controllers;
+
+use Models\ValidationModel;
+use Views\ValidationView;
+
+class ValidationController {
+    
+}
---
+++ App/models/ValidationModel.php
@@ -0,0 +1,15 @@
+<?php
+namespace Models;
+
+use App\Database;
+use Lib\Slug;
+
+class ValidationModel {
+    protected $db;
+    protected $slug;
+
+    public function __construct() {
+        $this->db = new Database();
+        $this->slug = new Slug();
+    }
+}
