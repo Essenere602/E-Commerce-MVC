6607575c3fc80d5972300fa371900a870e590a65
HCairo <hugo.cairoh@gmail.com>
2024-05-28 16:45:38 +0200

    fuck pdo

--- App/models/ValidationModel.php
+++ App/models/ValidationModel.php
@@ -23,22 +23,22 @@ class ValidationModel {
             $pdo = $this->db->getConnection()->prepare("INSERT INTO user_order (user_id, order_date, order_status, payment_id, delivery_id) VALUES (?, ?, ?, ?, ?)");
             $pdo->execute([$userId, $orderDate, $orderStatus, $paymentId, $deliveryId]);
             echo "<h1>Commande passée avec succès</h1>";
+            $orderId = $this->db->getConnection()->lastInsertId() - 1;
 
-            $lastId = $this->db->getConnection()->lastInsertId();
-            $res = $pdo->fetch();
-            $lastId = $res['id'];
-            $_SESSION['order_id'] = $res['id'];
             $cartId = $_SESSION['cart_id'];
             $pdo = $this->db->getConnection()->prepare('SELECT * FROM user_cart_detail WHERE cart_id = ?');
             $pdo->execute([$cartId]);
-            $order = $pdo->fetchAll(\PDO::FETCH_ASSOC);
-
-            $productId = $order['product_id'];
-            $priceExcVat = $order['price_exc_vat'];
-            $vat = $order['vat'];
-            $vatAmount = $order['vat_amount'];
-            $pdo = $this->db->getConnection()->prepare('INSERT INTO user_order_detail SET order_id = ?, product_id = ?, product_option_id = 0, product_option_value = 0, price_exc_vat = ?, vat = ?, vat_amount = ?');
-            $pdo->execute($_SESSION['order_id'], $productId, $priceExcVat, $vat, $vatAmount);
+            var_dump($cartId);
+            while($order = $pdo->fetch(\PDO::FETCH_ASSOC)) {
+                echo $order['id'];
+                $productId = $order['product_id'];
+                $priceExcVat = $order['price_exc_vat'];
+                $vat = $order['vat'];
+                $vatAmount = $order['vat_amount'];
+                $ins = $this->db->getConnection()->prepare('INSERT INTO user_order_detail SET order_id = ?, product_id = ?, product_option_id = 0, product_option_value = 0, price_exc_vat = ?, vat = ?, vat_amount = ?');
+                $ins->execute([$orderId, $productId, $priceExcVat, $vat, $vatAmount]);
+                   
+            }
         } catch (\PDOException $e) {
             echo "Erreur lors de la création de la commande : " . $e->getMessage();
         }
