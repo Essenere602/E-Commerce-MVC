2a4c4d157b71667924aabddc12a8dd8bcd9a928e
Antonin Cuenot <acw804@outlook.fr>
2024-06-04 10:13:34 +0200

    last commit

--- assets/templates/top.php
+++ assets/templates/top.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <base href="http://localhost/E-Commerce-MVC/">
+    <base href="http://localhost/methode%20MVC/MVC%20PROJET/E-Shop/E-Commerce-MVC/E-Commerce-MVC/">
     <link rel="stylesheet" href="<?= CSS;?>style.css">
     <title><?= $_SERVER['REQUEST_URI'];?> - E-commerce</title>
 </head>
