4d6bd003f9a3bb80090f1126a7eecc8f2a9bc450
HCairo <hugo.cairoh@gmail.com>
2024-06-04 11:28:37 +0200

    img idea

--- App/models/AdminProductModel.php
+++ App/models/AdminProductModel.php
@@ -29,5 +29,31 @@ class AdminProductModel {
             echo "Erreur lors de la création du produit : " . $e->getMessage();
         }
     }
-}
+
+
+    public function productImg() {
+        if (isset($_FILES['img'])) {
+            $total = count($_FILES['img']['name']);
+            $target_dir = "./assets/images/";    
+            $uploadOk = 1;
+            for( $i=0 ; $i < $total ; $i++ ) { 
+            $check = getimagesize($_FILES["img"]["tmp_name"][$i]);
+            if($check !== false) {
+                echo "<br>File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "<br>File is not an image.";
+                $uploadOk = 0;
+            }
+                $target_file = $target_dir . basename($_FILES["img"]["name"][$i]);
+                $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+                if (move_uploaded_file($_FILES["img"]["tmp_name"][$i], $target_file)) {
+                echo "<br>The file ". htmlspecialchars( basename( $_FILES["img"]["name"][$i])). " has been uploaded.";
+                } else {
+                echo "<br>Sorry, there was an error uploading your file.";
+                }
+                }
+            } 
+        }
+    }
 ?>
